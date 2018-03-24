e0c1ccfaf00f40f8f994d640d475833e6dea1e08
Mani Sedighi <manisedighi@apples-macbook-pro.local>
2018-03-24 16:48:19 +0100

    Initial Commit

---
+++ Mani Weather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mani Weather
+//
+//  Created by Mani Sedighi on 2018-03-24.
+//  Copyright © 2018 Mani Sedighi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
